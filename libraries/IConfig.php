cad0be8988f66bfb4a6b4189c18f169f1d191af5
Ibnu Toriq <ibnu@41studio.com>
2016-07-01 15:34:58 +0700

    Update IConfig.php

--- libraries/IConfig.php
+++ libraries/IConfig.php
@@ -5,7 +5,7 @@ interface IConfig
 {
 	const DB_HOST = 'localhost';
 	const DB_USER = 'root';
-	const DB_PASSWORD = 'caps123ee';
+	const DB_PASSWORD = 'password';
 	const DB_NAME = 'db_ecommerce';
 }
 ?>
